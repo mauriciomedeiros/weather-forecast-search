a712e74d25733e3a5503a1a43b6ac2a9a4f88784
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2016-06-24 19:47:08 -0300

    - Removed comment - Add Readme
     Please enter the commit message for your changes. Lines starting

---
+++ README.md
@@ -0,0 +1,8 @@
+#weather-forecast-search
+
+run ``` npm install ``` to download the dependencies
+
+run ``` npm start ``` up to the server
+
+
+Open your browser http://localhost:8080/#/weatherApp
--- app/js/controller/weatherController.js
+++ app/js/controller/weatherController.js
@@ -11,7 +11,6 @@
       vm.saerch = function(name){
          OpenWeatherMap.searchWeatherByCity(name).then(
             function success(response){
-               console.log(response);
                vm.forecast = angular.copy(response.data);
                vm.data = new Date(vm.forecast.dt * 1000);
             }
